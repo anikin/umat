82d8659a563c803bb6d18b6cbcc71223a96b262a
sever <sever@sever-a>
2016-05-16 16:08:23 +0300

    Bugfix

--- endpoints/advertiser_report_log_clicks.py
+++ endpoints/advertiser_report_log_clicks.py
@@ -2,9 +2,9 @@
 from .advertiser_report_log import AdvertiserReportLog
 
 
-class AdvertiserReportLogClics(AdvertiserReportLog):
+class AdvertiserReportLogClicks(AdvertiserReportLog):
     # I don't like long hierarchy of properties
     @property
     def export_url(self):
-        url = 'http://api.mobileapptracking.com/v3/logs/advertisers/{id}/exports/clics'
+        url = 'http://api.mobileapptracking.com/v3/logs/advertisers/{id}/exports/clicks'
         return url.format(id=self.advertiser_id)
