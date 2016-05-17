a411ac5cec6b37c20a0395e379a3f146951ced1c
sever <sever@sever-a>
2016-05-17 14:13:35 +0300

    Bugfix

--- __init__.py
+++ __init__.py
@@ -10,4 +10,4 @@ from .endpoints import (
 )
 
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
--- endpoints/params/v2.py
+++ endpoints/params/v2.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from base_params import Params as BaseParams
+from .base_params import Params as BaseParams
 
 
 class Params(BaseParams):
