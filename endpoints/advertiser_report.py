bcbe305879c5a586e03fc7597591445820de86af
sever <sever@sever-a>
2016-05-17 14:08:13 +0300

    Six!

--- endpoints/advertiser_report.py
+++ endpoints/advertiser_report.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 try:
-    from ConfigParser import ConfigParser
+    import configparser
 except:
-    from configparser import ConfigParser
-
+    from six.moves import configparser
 
 class AdvertiserReport(object):
     def __init__(self, config_filename):
-        self.config_parser = ConfigParser()
+        self.config_parser = configparser.ConfigParser()
         self.config_parser.read(config_filename)
         self.api_key = self.config_parser.get('main', 'api_key')
