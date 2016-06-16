778de20f43961b6252fd3588cd6415db70d4b13e
sever <sever@sever-a>
2016-06-16 12:28:13 +0300

    AdvertiserCampaigns

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -5,6 +5,7 @@ from .advertiser_report_log_clicks import AdvertiserReportLogClicks
 from .advertiser_report_log_opens import AdvertiserReportLogOpens
 from .advertiser_report_cohort_values import AdvertiserReportCohortValues
 from .advertiser_report_actuals import AdvertiserReportActuals
+from .advertiser_campaigns import AdvertiserCampaigns
 from .params.filter import Field
 from .service.export import MatExportError
 from .service.request import MatRequestError
